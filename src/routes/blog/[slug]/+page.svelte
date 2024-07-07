cd02778ef35e0919bffb329f5260620171265ad9
Shradesh Jodawat <shradeshjain123@gmail.com>
2024-07-07 11:14:05 +0530

    af

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -4,5 +4,7 @@
     export let data
 </script>
 
+<h1> Shradesh Jodawat</h1>
+
 <h1>{data.post.title}</h1>
 <div class="font-bold text-center text-5xl text-blue-300 hover:bg-slate-800 cursor-wait pt-5 pb-5 mt-5 mb-5">{@html data.post.content}</div>
